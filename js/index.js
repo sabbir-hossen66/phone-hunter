6cc32978fba511018d33d8cfa033b86822075494
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-03-01 03:34:44 +0600

    setup error handling

--- js/index.js
+++ js/index.js
@@ -8,5 +8,18 @@ const searchButton = () => {
 
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => allPhones(data.data))
+}
+
+const allPhones = info => {
+    const phoneContainer = document.getElementById('phone-container')
+    console.log(phoneContainer);
+    phoneContainer.textContent = '';
+
+    // show error messege..
+    if (info.length === 0) {
+        const errorMessege = document.getElementById('error');
+        errorMessege.innerText = "Sorry not available "
+    }
+
 }
