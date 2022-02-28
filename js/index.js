329a2d7cc6b988bf2e6deb7846f97e96c919d771
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-03-01 03:09:42 +0600

    url added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+
 const searchButton = () => {
     const input = document.getElementById('input-value')
-    inputText = input.value
-    console.log(inputText);
+    inputText = input.value;
+    input.value = '';
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchButton}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data))
 }
