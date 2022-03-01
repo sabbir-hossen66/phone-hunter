84f8c6ccc9dda5d4609bf2123745dc51526c3526
sabbir-2000 <sabbir.ms2000@gmail.com>
2022-03-01 17:38:17 +0600

    slice added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+document.getElementById('error').style.display = 'none'
 const main = document.getElementById('main');
 const searchButton = () => {
     const input = document.getElementById('input-value')
@@ -5,50 +6,107 @@ const searchButton = () => {
     inputText = input.value;
     input.value = '';
 
-    if (inputText.length > 0) {
-        // main.innerHTML = '';
-        errorMessege.innerText = "Sorry not available "
+    if (parseFloat(inputText) < 0) {
+
+        errorMessege.innerText = "Sorry not available ";
+        input.value = '';
+        main.innerHTML = '';
 
 
     }
 
     else {
+        main.innerHTML = '';
+
 
         const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
 
         fetch(url)
             .then(res => res.json())
-            .then(data => phoneDisplay(data.data))
+            .then(data => phoneDisplay(data.data.slice(0, 20)))
     }
 }
 // for (const phone of info) 
 const phoneDisplay = info => {
     console.log(info);
+    /* error handler */
+    if (info.length == 0) {
+        document.getElementById('error').style.display = 'block'
+
+
+    }
+
+
+
     info.forEach(phone => {
-        console.log(phone.slug);
-        const div = document.createElement('div')
-        div.className = ('col col-lg-4');
-        div.className = ('mb-5');
+        document.getElementById('error').style.display = 'none'
+        // console.log(phone.slug);
+        const div = document.createElement('div');
+        div.className = ('col-lg-4 mb-5');
+
         div.innerHTML = `
         <div class="card" style="width: 18rem;">
   <img src="${phone.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${phone.phone_name}</h5>
     <h4 class='card-title'>${phone.brand}</h4>
-    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">See Details</button>
+    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-warning">See Details</button>
   </div>
 </div>
         `;
         main.appendChild(div)
     })
 
+
+
 }
 
 
 const phoneDetails = id => {
+
+
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data[0]))
+        .then(data => displayPhoneDetails(data.data))
+
+
 }
 
+
+const displayPhoneDetails = data => {
+    console.log(data);
+    const rate = () => {
+        const newName = data.mainFeatures.sensors.map(sensor => sensor)
+        return newName;
+    }
+
+
+
+
+    const onePhone = document.getElementById('one-phone');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+
+      <img src="${data.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${data.name}</h5>
+        <p class="card-text">${data.releaseDate}</p>
+        <p class="card-title">${data.brand}</p>
+        <p>${rate()}</p>
+        <a href="${data.chipSet}" class="btn btn-primary">Go somewhere</a>
+      </div>
+
+
+        `;
+    onePhone.appendChild(div);
+
+
+
+
+
+
+    // onePhone.textContent = '';
+
+}
